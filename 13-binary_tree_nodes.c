f62eb3214b0eaca7f603ec271e533dbc5295b9c2
hadjara <hadjaramamane61@gmail.com>
2023-08-03 18:32:49 -0700

    task 0-18

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 #include <stdio.h>
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree: tree to count the nodes from
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,19 +1,11 @@
 #include "binary_trees.h"
-
+#include <stdio.h>
 /**
  * binary_tree_insert_right - inserts a node as the
  *					right-child of another node
- *
  * @parent: is a pointer to the node to insert the right-child in
  * @value: is the value to store in the new node
- *
- * Description: If parent already has a right-child, the new node
- *				must take its place, and the old right-child must
- *				be set as the right-child of the new node.
- *
- * Return: a pointer to the created node, or
- *			NULL on failure or
- *			if parent is NULL
+ * Return: a pointer or NULL on failure if parent is NULL
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
