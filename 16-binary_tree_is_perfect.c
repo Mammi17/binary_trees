1f4bceceeed607fdb515bb18dfde1fe3b6b81e99
hadjara <hadjaramamane61@gmail.com>
2023-08-03 18:42:54 -0700

    task 0-18

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,37 @@
 #include "binary_trees.h"
 #include <stdio.h>
 /**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ * Return: 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left = 0;
+	size_t h_right = 0;
+
+	if (!tree)
+		return (0);
+
+	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (h_left > h_right ? h_left : h_right);
+}
+/**
  *pow_recur - returns the value of x raised to the power of y
  *@x: the value to exponentiate
  *@y: the power to raise x to
